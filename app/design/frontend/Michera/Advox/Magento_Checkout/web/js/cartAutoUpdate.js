50bcbeda2277ff2de005ca4d122bee446b423b8f
antmic <antoni.michera@gmail.com>
2023-05-01 23:02:33 +0200

    4th task. Final touches.

--- app/design/frontend/Michera/Advox/Magento_Checkout/web/js/cartAutoUpdate.js
+++ app/design/frontend/Michera/Advox/Magento_Checkout/web/js/cartAutoUpdate.js
@@ -3,8 +3,7 @@ define([
 ], function cartAutoUpdate($, getTotalsAction, customerData) {
     return function (config, element) {
 
-        console.log('test4')
-
+        // Restores focus after reload.
         let activeElementId = localStorage.getItem('activeElementId');
         if (activeElementId) {
             let activeElement = document.getElementById(activeElementId);
@@ -15,14 +14,27 @@ define([
             localStorage.removeItem('activeElementId');
         }
 
+        // Variables
         const inputElement = document.querySelector(config.cartInput);
         let inputType;
         let inputData;
         let pasteData;
 
-        inputElement.addEventListener('input', (e) => updateCart(e))
+        // Event listeners
+        const debouncedUpdateCart = debounce(updateCart);
+        inputElement.addEventListener('input', (e) => debouncedUpdateCart(e))
         inputElement.addEventListener('paste', (e) => checkPaste(e))
 
+        // This is a workaround for a bug or "feature" in macOS.
+        // Handles macOS specific quick double space changing to a dot -> now it clears the input.
+        $(inputElement).on('keydown', (e) => {
+            if (e.which === 32) {
+                inputElement.value = inputElement.value.replace(/\s/g, '');
+                }
+            }
+        );
+
+        // Validate paste input.
         function checkPaste(e) {
             let clipboardData = e.clipboardData || e.originalEvent.clipboardData || window.clipboardData;
             pasteData = clipboardData.getData('text');
@@ -32,6 +44,7 @@ define([
             }
         }
 
+        // Validate all inputs to prevent reload if any out-of-focus input is invalid.
         function validateAllInputs() {
             let allInputs = [];
             for (let i = 0; i < $('input.qty').length; i++) {
@@ -40,16 +53,22 @@ define([
             return allInputs.every(Number);
         }
 
-        function updateCart(e) {
+        function debounce(func, timeout = 300) {
+            let timer;
+            return (...args) => {
+                clearTimeout(timer);
+                timer = setTimeout(() => {
+                    func.apply(this, args);
+                }, timeout);
+            };
+        }
 
+        function updateCart(e) {
             activeElementId = document.activeElement.id
             localStorage.setItem('activeElementId', activeElementId)
-
             const form = $('form#form-validate');
-
             inputData = e.data;
             inputType = e.inputType;
-
             if ((/[0-9]/.test(inputData) || /[0-9]/.test(pasteData) || inputType === 'deleteContentBackward' || inputType === 'deleteContentForward') && validateAllInputs()) {
                 $.ajax({
                     url: form.attr('action'), data: form.serialize(), showLoader: true, success: function (res) {
